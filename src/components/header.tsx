47e88f66b2f8d5df4f14d3de1a6b15293bcce0a0
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-04 22:23:22 +0100

    feat(P-01): add header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className="bg-black sticky text-white top-1 z-50">
-      <nav className="container mx-auto px-6 py-4">
+      <nav className="px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-4">
             <img
@@ -10,9 +10,44 @@ export function Header() {
               className="w-12 h-12 rounded-full border-0"
             />
             <div>
-                <h1 className="text-xl font-bold">D0M1N1K_.</h1>
+              <h1 className="text-xl font-bold">D0M1N1K_.</h1>
             </div>
           </div>
+
+          <ul className="flex gap-12 mr-20">
+            <li>
+              <a
+                href="#home"
+                className="hover:text-blue-500 transition duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skills"
+                className="hover:text-blue-500 transition duration-300"
+              >
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#projects"
+                className="hover:text-blue-500 transition duration-300"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="hover:text-blue-500 transition duration-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
