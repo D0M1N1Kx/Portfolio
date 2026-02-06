cff85306c032f17858c5bbf823756343a9ac885a
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-06 21:19:15 +0100

    style(P-02): ProjectCard got a rounded corner on top of the image

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -14,7 +14,7 @@ export function ProjectCard({
   return (
     <>
       <div className="border-b-2 border-sky-700 w-86 h-fit bg-blue-600/15 rounded-lg mb-4">
-        <img src={img_path} width={400} height={180} />
+        <img src={img_path} width={400} height={180} className="rounded-t-lg"/>
         <h1 className="text-center font-bold text-2xl mt-1 mb-1">{title}</h1>
         <span className="font-bold text-base">{description}</span>
         <br></br>
