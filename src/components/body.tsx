99ab38860cf5ef09dc7c54a1423d8a745592f4b4
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-06 20:02:03 +0100

    refactor(P-02): add Skills section to body and remove old skill card component

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,5 +1,3 @@
-import SkillCard from "./SkillCard";
-
 export function Body() {
   const skills = [
     {
@@ -48,18 +46,9 @@ export function Body() {
           </div>
         </div>
         <div className="h-0.5 bg-blue-800 w-full mb-3 mt-10"></div>
-
-        <h1 className="text-3xl text-center md:text-4xl lg:text-5xl font-bold text-blue-400 drop-shadow-[0_0_15px_rgba(96,165,250,0.8)]">
+        <h1 className="text-sky-600 font-bold text-4xl text-center mt-4">
           Skills
         </h1>
-        <div className="flex justify-center my-6">
-          <hr className="w-3/12 border-t-2 border-blue-500 shadow-[0_0_10px_rgba(96,165,250,0.8)]" />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-          {skills.map((skill, index) => (
-            <SkillCard key={index} title={skill.title} items={skill.items} />
-          ))}
-        </div>
       </div>
     </>
   );
