b49b41f23c96923ff454913bc4e5a90daed2ee07
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-27 15:09:49 +0200

    fix: fix skillCard height and width

--- src/components/skillCard.tsx
+++ src/components/skillCard.tsx
@@ -6,7 +6,7 @@ type SkillCardParameters = {
 export function SkillCard({ title, items }: SkillCardParameters) {
   return (
     <>
-      <div className="border-2 border-sky-700 w-86 min-h-44 h-auto py-6 m-1 bg-blue-600/15 rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
+      <div className="h-52 w-50 py-6 px-1 m-1 border-2 border-sky-700 bg-blue-600/15 rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer overflow-hidden">
         <h3 className="text-center font-bold text-blue-500 text-2xl">
           {title}
         </h3>
