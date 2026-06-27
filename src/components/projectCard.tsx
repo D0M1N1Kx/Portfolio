24f7f952065bf76240dd11440616a75835d98cd9
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-27 16:12:06 +0200

    restyle: restyle projectCard.tsx

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -12,21 +12,31 @@ export function ProjectCard({
   link,
 }: projectCardParameters) {
   return (
-    <>
-      <div className="border-b-2 border-sky-700 w-86 h-fit m-1 bg-blue-600/15 rounded-lg mb-4 hover:border-b-4 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer">
-        <img src={img_path} width={400} height={180} className="rounded-t-lg w-full"/>
-        <h1 className="text-center font-bold text-2xl mt-1 mb-1">{title}</h1>
-        <span className="font-bold text-base">{description}</span>
-        <br></br>
-        <button
-          className="rounded-2xl px-4 py-2 mt-4 mb-3 font-semibold bg-blue-600 text-white border-blue-700 hover:bg-blue-700/90 hover:scale-105 transition-all duration-300"
-          onClick={() => {
-            window.open(link);
-          }}
-        >
-          Source
-        </button>
-      </div>
-    </>
+      <>
+        <div className="flex flex-col justify-between border-b-2 border-sky-700 w-full sm:w-86 min-h-[400px] m-1 bg-blue-600/15 rounded-lg mb-4 hover:border-b-4 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer overflow-hidden">
+
+          <div>
+            <div className="h-44 overflow-hidden">
+              <img src={img_path} className="rounded-t-lg w-full h-full object-cover"/>
+            </div>
+            <div className="p-4">
+              <h1 className="text-center font-bold text-2xl mb-2">{title}</h1>
+              <p className="text-base text-gray-200 line-clamp-3">{description}</p>
+            </div>
+          </div>
+
+          <div className="px-4 pb-4">
+            <button
+                className="w-full sm:w-auto rounded-2xl px-4 py-2 font-semibold bg-blue-600 text-white border-blue-700 hover:bg-blue-700/90 hover:scale-105 transition-all duration-300"
+                onClick={() => {
+                  window.open(link);
+                }}
+            >
+              Source
+            </button>
+          </div>
+
+        </div>
+      </>
   );
 }
